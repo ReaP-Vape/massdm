d46adcc1e210e3e331f4fae6d88de35b0c330781
ReaP-Vape <49915708+ReaP-Vape@users.noreply.github.com>
2021-05-03 14:30:12 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 echo Don't forget to run install_dep.bat prior to starting^!
 title MassDM Discord Bot
-REM set "BOT_TOKEN=YOUR TOKEN"
+REM set "NzQ5MDg0NjMzODQ0Njc4NjY2.X0m1bg.81K8D3OlY8oYTZ0qlHtTwSbdAPo"
 node app.js --harmony
 pause
